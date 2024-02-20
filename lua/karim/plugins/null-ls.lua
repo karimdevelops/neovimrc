a77bfe496ea7323a4eb2327a8fbe2416c398d9b1
Abdul Karim <karim.develops@gmail.com>
2024-02-20 21:49:18 +0500

    Add lsp and snippets config

---
+++ lua/karim/plugins/null-ls.lua
@@ -0,0 +1,45 @@
+return {
+	"jay-babu/mason-null-ls.nvim",
+	event = { "BufReadPre", "BufNewFile" },
+	dependencies = {
+		"williamboman/mason.nvim",
+		"nvimtools/none-ls.nvim",
+	},
+	config = function()
+		local null_ls = require("null-ls")
+		local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+		null_ls.setup({
+			require("mason-null-ls").setup({
+				ensure_installed = {
+					"stylua",
+					"goimports",
+					"gofumpt",
+					"gomodifytags",
+					"impl",
+					"delve",
+					"prettier",
+				},
+			}),
+			on_attach = function(client, bufnr)
+				if client.supports_method("textDocument/formatting") then
+					vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+					vim.api.nvim_create_autocmd("BufWritePre", {
+						group = augroup,
+						buffer = bufnr,
+						callback = function()
+							vim.lsp.buf.format()
+						end,
+					})
+				end
+			end,
+			sources = {
+				null_ls.builtins.formatting.stylua,
+				null_ls.builtins.code_actions.gomodifytags,
+				null_ls.builtins.code_actions.impl,
+				null_ls.builtins.formatting.goimports,
+				null_ls.builtins.formatting.gofumpt,
+				null_ls.builtins.formatting.prettier,
+			},
+		})
+	end,
+}
