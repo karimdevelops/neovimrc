ce18a58dd3add937460ee8432d4cdb9088e61188
Abdul Karim <karim.develops@gmail.com>
2024-02-20 21:46:26 +0500

    Add editor settings config

---
+++ lua/karim/settings.lua
@@ -0,0 +1,16 @@
+vim.g.mapleader = " "
+vim.opt.nu = true
+vim.opt.relativenumber = true
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.o.undofile = true
+
+vim.g.netrw_browse_split = 0
+vim.g.netrw_banner = 0
+
+vim.opt.signcolumn = "yes"
+vim.opt.colorcolumn = "80"
